de769cf4311ab7a7b0b689288d3fabea2c1192ce
maxwell fundi <maxwellfundi@live.com>
2019-10-02 17:44:41 +0300

    This is the first addition of the project files

---
+++ app/src/main/java/com/farm/farmapp/ActivityEntry7.java
@@ -0,0 +1,13 @@
+package com.farm.farmapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityEntry7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry7);
+    }
+}
